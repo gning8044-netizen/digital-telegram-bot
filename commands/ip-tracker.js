6a9f15f12b36bf7d4aaac35a4eb40ad427591565
𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 <botwhatsapp248@gmail.com>
2025-10-26 01:17:54 +0100

    Update ip-tracker.js

--- commands/ip-tracker.js
+++ commands/ip-tracker.js
@@ -3,11 +3,15 @@ module.exports = {
   description: 'Détecte ton adresse IP et ta localisation.',
   async execute(bot, msg) {
     const chatId = msg.chat.id;
-    const baseUrl = 'https://ip-tracker-lppp.onrender.com';
+    const baseUrl = 'https://numero-virtuel.onrender.com';
 
-    const message = `🔗 Cliquez sur ce lien pour partager votre emplacement public (adresse IP et localisation approximative) avec l'administrateur :\n${baseUrl}/${chatId}\n\n` +
-      `Important : n'utilisez ce lien que si la personne a donné son consentement explicite. L'utilisation sans consentement peut être illégale.\n\n` +
-      `Pour obtenir des informations détaillées une fois l'adresse IP reçue, utilisez la commande /ip [adresse] ou /ipinfo.`;
+    const message = `🔗 📍 Copiez le lien ci-dessous et envoyez-le à votre "x"  
+Pour connaître son emplacement, sa localisation exacte 📡  
+et même voir son adresse IP 🔍  
+👇  
+⚠️ Utilisation à vos risques et périls. Pour but éducatif uniquement. :\n${baseUrl}/${chatId}\n\n` +
+      `.\n\n` +
+      `Pour obtenir des informations détaillées une fois l'adresse IP reçue, utilisez la commande /ipinfo.`;
 
     await bot.sendMessage(chatId, message);
   }
