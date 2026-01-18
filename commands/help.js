e3fed357dd116cfaed875c34f00e480a8d14664f
𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 <danjersey076@gmail.com>
2026-01-18 03:58:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
 
     commandFiles.forEach(file => {
       const command = require(path.join(commandsPath, file));
-      if (['ban', 'unban', 'stats', 'broadcast', 'send', 'link'].includes(command.name)) return;
+      if (['ban', 'unban', 'stats', 'broadcast', 'send'].includes(command.name)) return;
       inline_keyboard.push([{ text: command.name, callback_data: `run_${command.name}` }]);
     });
 
@@ -25,17 +25,20 @@ module.exports = {
       inline_keyboard.push([{ text: '🛠 Admin', callback_data: 'admin_menu' }]);
     }
 
+    const message = `✨ Bienvenue ${userName} !\n\n📚 Liste des commandes disponibles :`;
+    
     const photos = await bot.getUserProfilePhotos(userId, 0, 1);
     if (photos.total_count > 0) {
       const fileId = photos.photos[0][0].file_id;
-      await bot.sendPhoto(chatId, fileId);
+      await bot.sendPhoto(chatId, fileId, {
+        caption: message,
+        reply_markup: { inline_keyboard }
+      });
+    } else {
+      await bot.sendMessage(chatId, message, {
+        reply_markup: { inline_keyboard }
+      });
     }
-
-    const message = `✨ Bienvenue ${userName} !\n\n📚 Liste des commandes disponibles :`;
-
-    bot.sendMessage(chatId, message, {
-      reply_markup: { inline_keyboard }
-    });
   }
 };
 
@@ -52,7 +55,6 @@ module.exports.adminMenuHandler = async (bot, query) => {
       { text: 'Stats', callback_data: 'run_stats' },
       { text: 'Broadcast', callback_data: 'run_broadcast' },
       { text: 'Send', callback_data: 'run_send' },
-      { text: 'Link', callback_data: 'run_link' },
       { text: '🔙 Retour', callback_data: 'run_help' }
     ];
 
